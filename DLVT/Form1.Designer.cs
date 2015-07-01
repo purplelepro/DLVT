c6b26dc2d15f342deecadc58b041203546bdaa86
VJ <purplelepro@unseen.is>
2015-07-01 02:30:53 -0400

    Init

---
+++ DLVT/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace DLVT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iTalk_ThemeContainer1 = new iTalk.iTalk_ThemeContainer();
+            this.iTalk_Button_24 = new iTalk.iTalk_Button_2();
+            this.iTalk_TextBox_Small3 = new iTalk.iTalk_TextBox_Small();
+            this.iTalk_Label8 = new iTalk.iTalk_Label();
+            this.iTalk_Label7 = new iTalk.iTalk_Label();
+            this.iTalk_Label6 = new iTalk.iTalk_Label();
+            this.iTalk_Label5 = new iTalk.iTalk_Label();
+            this.iTalk_Label4 = new iTalk.iTalk_Label();
+            this.iTalk_ControlBox1 = new iTalk.iTalk_ControlBox();
+            this.iTalk_Button_23 = new iTalk.iTalk_Button_2();
+            this.iTalk_Label3 = new iTalk.iTalk_Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.iTalk_Label2 = new iTalk.iTalk_Label();
+            this.iTalk_Label1 = new iTalk.iTalk_Label();
+            this.iTalk_TextBox_Small2 = new iTalk.iTalk_TextBox_Small();
+            this.iTalk_TextBox_Small1 = new iTalk.iTalk_TextBox_Small();
+            this.iTalk_Button_22 = new iTalk.iTalk_Button_2();
+            this.iTalk_Button_21 = new iTalk.iTalk_Button_2();
+            this.iTalk_ThemeContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // iTalk_ThemeContainer1
+            // 
+            this.iTalk_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Button_24);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_TextBox_Small3);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label8);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label7);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label6);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label5);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label4);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_ControlBox1);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Button_23);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label3);
+            this.iTalk_ThemeContainer1.Controls.Add(this.listBox1);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label2);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label1);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_TextBox_Small2);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_TextBox_Small1);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Button_22);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Button_21);
+            this.iTalk_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.iTalk_ThemeContainer1.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_ThemeContainer1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_ThemeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.iTalk_ThemeContainer1.Name = "iTalk_ThemeContainer1";
+            this.iTalk_ThemeContainer1.Padding = new System.Windows.Forms.Padding(3, 28, 3, 28);
+            this.iTalk_ThemeContainer1.Sizable = true;
+            this.iTalk_ThemeContainer1.Size = new System.Drawing.Size(829, 288);
+            this.iTalk_ThemeContainer1.SmartBounds = false;
+            this.iTalk_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.iTalk_ThemeContainer1.TabIndex = 0;
+            this.iTalk_ThemeContainer1.Text = "DLVT";
+            // 
+            // iTalk_Button_24
+            // 
+            this.iTalk_Button_24.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Button_24.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.iTalk_Button_24.ForeColor = System.Drawing.Color.White;
+            this.iTalk_Button_24.Image = null;
+            this.iTalk_Button_24.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iTalk_Button_24.Location = new System.Drawing.Point(555, 146);
+            this.iTalk_Button_24.Name = "iTalk_Button_24";
+            this.iTalk_Button_24.Size = new System.Drawing.Size(128, 40);
+            this.iTalk_Button_24.TabIndex = 16;
+            this.iTalk_Button_24.Text = "Test";
+            this.iTalk_Button_24.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.iTalk_Button_24.Click += new System.EventHandler(this.iTalk_Button_24_Click);
+            // 
+            // iTalk_TextBox_Small3
+            // 
+            this.iTalk_TextBox_Small3.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_TextBox_Small3.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.iTalk_TextBox_Small3.ForeColor = System.Drawing.Color.DimGray;
+            this.iTalk_TextBox_Small3.Location = new System.Drawing.Point(343, 222);
+            this.iTalk_TextBox_Small3.MaxLength = 32767;
+            this.iTalk_TextBox_Small3.Multiline = false;
+            this.iTalk_TextBox_Small3.Name = "iTalk_TextBox_Small3";
+            this.iTalk_TextBox_Small3.ReadOnly = false;
+            this.iTalk_TextBox_Small3.Size = new System.Drawing.Size(135, 28);
+            this.iTalk_TextBox_Small3.TabIndex = 15;
+            this.iTalk_TextBox_Small3.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.iTalk_TextBox_Small3.UseSystemPasswordChar = false;
+            // 
+            // iTalk_Label8
+            // 
+            this.iTalk_Label8.AutoSize = true;
+            this.iTalk_Label8.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label8.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label8.Location = new System.Drawing.Point(440, 182);
+            this.iTalk_Label8.Name = "iTalk_Label8";
+            this.iTalk_Label8.Size = new System.Drawing.Size(13, 13);
+            this.iTalk_Label8.TabIndex = 14;
+            this.iTalk_Label8.Text = "0";
+            // 
+            // iTalk_Label7
+            // 
+            this.iTalk_Label7.AutoSize = true;
+            this.iTalk_Label7.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label7.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label7.Location = new System.Drawing.Point(440, 160);
+            this.iTalk_Label7.Name = "iTalk_Label7";
+            this.iTalk_Label7.Size = new System.Drawing.Size(26, 13);
+            this.iTalk_Label7.TabIndex = 13;
+            this.iTalk_Label7.Text = "N/A";
+            // 
+            // iTalk_Label6
+            // 
+            this.iTalk_Label6.AutoSize = true;
+            this.iTalk_Label6.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label6.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label6.Location = new System.Drawing.Point(343, 206);
+            this.iTalk_Label6.Name = "iTalk_Label6";
+            this.iTalk_Label6.Size = new System.Drawing.Size(63, 13);
+            this.iTalk_Label6.TabIndex = 12;
+            this.iTalk_Label6.Text = "Url Report:";
+            // 
+            // iTalk_Label5
+            // 
+            this.iTalk_Label5.AutoSize = true;
+            this.iTalk_Label5.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label5.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label5.Location = new System.Drawing.Point(343, 182);
+            this.iTalk_Label5.Name = "iTalk_Label5";
+            this.iTalk_Label5.Size = new System.Drawing.Size(96, 13);
+            this.iTalk_Label5.TabIndex = 11;
+            this.iTalk_Label5.Text = "Detected Results:";
+            // 
+            // iTalk_Label4
+            // 
+            this.iTalk_Label4.AutoSize = true;
+            this.iTalk_Label4.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label4.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label4.Location = new System.Drawing.Point(343, 160);
+            this.iTalk_Label4.Name = "iTalk_Label4";
+            this.iTalk_Label4.Size = new System.Drawing.Size(91, 13);
+            this.iTalk_Label4.TabIndex = 10;
+            this.iTalk_Label4.Text = "Scanned Before:";
+            // 
+            // iTalk_ControlBox1
+            // 
+            this.iTalk_ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.iTalk_ControlBox1.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_ControlBox1.Location = new System.Drawing.Point(748, -1);
+            this.iTalk_ControlBox1.Name = "iTalk_ControlBox1";
+            this.iTalk_ControlBox1.Size = new System.Drawing.Size(77, 19);
+            this.iTalk_ControlBox1.TabIndex = 9;
+            this.iTalk_ControlBox1.Text = "iTalk_ControlBox1";
+            // 
+            // iTalk_Button_23
+            // 
+            this.iTalk_Button_23.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Button_23.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.iTalk_Button_23.ForeColor = System.Drawing.Color.White;
+            this.iTalk_Button_23.Image = null;
+            this.iTalk_Button_23.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iTalk_Button_23.Location = new System.Drawing.Point(689, 146);
+            this.iTalk_Button_23.Name = "iTalk_Button_23";
+            this.iTalk_Button_23.Size = new System.Drawing.Size(128, 40);
+            this.iTalk_Button_23.TabIndex = 8;
+            this.iTalk_Button_23.Text = "Browse Save";
+            this.iTalk_Button_23.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // iTalk_Label3
+            // 
+            this.iTalk_Label3.AutoSize = true;
+            this.iTalk_Label3.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label3.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label3.Location = new System.Drawing.Point(12, 146);
+            this.iTalk_Label3.Name = "iTalk_Label3";
+            this.iTalk_Label3.Size = new System.Drawing.Size(74, 13);
+            this.iTalk_Label3.TabIndex = 7;
+            this.iTalk_Label3.Text = "Scan Results:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 162);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(325, 95);
+            this.listBox1.TabIndex = 6;
+            // 
+            // iTalk_Label2
+            // 
+            this.iTalk_Label2.AutoSize = true;
+            this.iTalk_Label2.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label2.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label2.Location = new System.Drawing.Point(12, 88);
+            this.iTalk_Label2.Name = "iTalk_Label2";
+            this.iTalk_Label2.Size = new System.Drawing.Size(77, 13);
+            this.iTalk_Label2.TabIndex = 5;
+            this.iTalk_Label2.Text = "Save Locaton:";
+            // 
+            // iTalk_Label1
+            // 
+            this.iTalk_Label1.AutoSize = true;
+            this.iTalk_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label1.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label1.Location = new System.Drawing.Point(12, 38);
+            this.iTalk_Label1.Name = "iTalk_Label1";
+            this.iTalk_Label1.Size = new System.Drawing.Size(87, 13);
+            this.iTalk_Label1.TabIndex = 4;
+            this.iTalk_Label1.Text = "Download URL:";
+            // 
+            // iTalk_TextBox_Small2
+            // 
+            this.iTalk_TextBox_Small2.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_TextBox_Small2.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.iTalk_TextBox_Small2.ForeColor = System.Drawing.Color.DimGray;
+            this.iTalk_TextBox_Small2.Location = new System.Drawing.Point(12, 104);
+            this.iTalk_TextBox_Small2.MaxLength = 32767;
+            this.iTalk_TextBox_Small2.Multiline = false;
+            this.iTalk_TextBox_Small2.Name = "iTalk_TextBox_Small2";
+            this.iTalk_TextBox_Small2.ReadOnly = false;
+            this.iTalk_TextBox_Small2.Size = new System.Drawing.Size(805, 28);
+            this.iTalk_TextBox_Small2.TabIndex = 3;
+            this.iTalk_TextBox_Small2.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.iTalk_TextBox_Small2.UseSystemPasswordChar = false;
+            // 
+            // iTalk_TextBox_Small1
+            // 
+            this.iTalk_TextBox_Small1.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_TextBox_Small1.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.iTalk_TextBox_Small1.ForeColor = System.Drawing.Color.DimGray;
+            this.iTalk_TextBox_Small1.Location = new System.Drawing.Point(12, 54);
+            this.iTalk_TextBox_Small1.MaxLength = 32767;
+            this.iTalk_TextBox_Small1.Multiline = false;
+            this.iTalk_TextBox_Small1.Name = "iTalk_TextBox_Small1";
+            this.iTalk_TextBox_Small1.ReadOnly = false;
+            this.iTalk_TextBox_Small1.Size = new System.Drawing.Size(805, 28);
+            this.iTalk_TextBox_Small1.TabIndex = 2;
+            this.iTalk_TextBox_Small1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.iTalk_TextBox_Small1.UseSystemPasswordChar = false;
+            // 
+            // iTalk_Button_22
+            // 
+            this.iTalk_Button_22.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Button_22.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.iTalk_Button_22.ForeColor = System.Drawing.Color.White;
+            this.iTalk_Button_22.Image = null;
+            this.iTalk_Button_22.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iTalk_Button_22.Location = new System.Drawing.Point(689, 217);
+            this.iTalk_Button_22.Name = "iTalk_Button_22";
+            this.iTalk_Button_22.Size = new System.Drawing.Size(128, 40);
+            this.iTalk_Button_22.TabIndex = 1;
+            this.iTalk_Button_22.Text = "Scan File";
+            this.iTalk_Button_22.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.iTalk_Button_22.Click += new System.EventHandler(this.iTalk_Button_22_Click);
+            // 
+            // iTalk_Button_21
+            // 
+            this.iTalk_Button_21.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Button_21.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.iTalk_Button_21.ForeColor = System.Drawing.Color.White;
+            this.iTalk_Button_21.Image = null;
+            this.iTalk_Button_21.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iTalk_Button_21.Location = new System.Drawing.Point(555, 217);
+            this.iTalk_Button_21.Name = "iTalk_Button_21";
+            this.iTalk_Button_21.Size = new System.Drawing.Size(128, 40);
+            this.iTalk_Button_21.TabIndex = 0;
+            this.iTalk_Button_21.Text = "Download File";
+            this.iTalk_Button_21.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 288);
+            this.Controls.Add(this.iTalk_ThemeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(126, 39);
+            this.Name = "Form1";
+            this.Text = "DLVT";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.iTalk_ThemeContainer1.ResumeLayout(false);
+            this.iTalk_ThemeContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private iTalk.iTalk_ThemeContainer iTalk_ThemeContainer1;
+        private iTalk.iTalk_TextBox_Small iTalk_TextBox_Small2;
+        private iTalk.iTalk_TextBox_Small iTalk_TextBox_Small1;
+        private iTalk.iTalk_Button_2 iTalk_Button_22;
+        private iTalk.iTalk_Button_2 iTalk_Button_21;
+        private iTalk.iTalk_Button_2 iTalk_Button_23;
+        private iTalk.iTalk_Label iTalk_Label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private iTalk.iTalk_Label iTalk_Label2;
+        private iTalk.iTalk_Label iTalk_Label1;
+        private iTalk.iTalk_ControlBox iTalk_ControlBox1;
+        private iTalk.iTalk_Label iTalk_Label8;
+        private iTalk.iTalk_Label iTalk_Label7;
+        private iTalk.iTalk_Label iTalk_Label6;
+        private iTalk.iTalk_Label iTalk_Label5;
+        private iTalk.iTalk_Label iTalk_Label4;
+        private iTalk.iTalk_TextBox_Small iTalk_TextBox_Small3;
+        private iTalk.iTalk_Button_2 iTalk_Button_24;
+    }
+}
+
---
+++ DLVT/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VirusTotalNET;
+using VirusTotalNET.Objects;
+using System.IO;
+
+namespace DLVT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void iTalk_Button_22_Click(object sender, EventArgs e)
+        {
+            // initializing VT with API key
+            VirusTotal VT = new VirusTotal("c9836f1fb3196ec1205c4eb4f58cc50be1b41915f2b18633927e8a7333457cc8");
+
+            // Yes we want to use HTTPS instead of HTTP
+            VT.UseTLS = true;
+
+            // Getting our file 
+            FileInfo fileInfo = new FileInfo("asdf.txt"/*iTalk_TextBox_Small2.Text*/);
+
+            //debug purpose 
+            File.WriteAllText(fileInfo.FullName, @"purpletestfislekkkk");
+
+            // Getting our fileReport
+            FileReport fileReport = VT.GetFileReport(fileInfo);
+
+            // Checking weather or not there is a previous scan on the file
+            bool prevScan = fileReport.ResponseCode == ReportResponseCode.Present;
+            
+            // If there was a previous scan or if prevScan = true ( its a bool )
+            // Update our label to let the users know there is a pervious scan
+            // else
+            // We want to upload the file and get the scan report
+            // ( this is where PrintScan(ScanResult scanresult) comes into play ) 
+            if (prevScan)
+            {
+                PrintScan(fileReport);
+                iTalk_Label7.Text = "YES";
+                iTalk_Label7.ForeColor = Color.Green;
+            }
+            else
+            {
+                ScanResult fileReport2 = VT.ScanFile(fileInfo);
+                PrintScan(fileReport2);
+                iTalk_Label7.Text = "NO";
+                iTalk_Label7.ForeColor = Color.Red;
+            }
+            
+        }
+
+        // Goto VT Button
+        // Takes the user to the virustotal site of their scanned file
+        private void iTalk_Button_24_Click(object sender, EventArgs e)
+        {
+            // Checking to make sure ScanID isn't empty
+            if (!String.IsNullOrEmpty(iTalk_TextBox_Small3.Text))
+            {
+                string VTScanID = iTalk_TextBox_Small3.Text;
+                //Removing the past 11 chars or so 
+                // E.g ScanID 1e5408577b802de6aaed8240f3c4c2b8b0b3fee9d1fcafef84bdb8c716181b88-1435728995
+                // We don't need the -######### so lets just remove everything from it
+                string VTscanUrl = VTScanID.Remove(64);
+                System.Diagnostics.Process.Start("https://www.virustotal.com/en/file/" + VTscanUrl + "/analysis/");
+            }
+            else
+            {
+                MessageBox.Show("You must first scan the file!");
+            }
+        }
+
+        // if there isn't an active scan report already on the file
+        // we want to go a head and grab the ScanID from it
+        // and apply it to our Scan URL textbox.
+        // this way we can either A: re-scan and get the updated
+        // detections list or click VT Button and visit the site
+        // directly.
+        private void PrintScan(ScanResult scanResult)
+        {
+             iTalk_TextBox_Small3.Text = scanResult.ScanId;
+        }
+
+        // Grabs the report and allows us to access some information
+        // We add +1 for each detection to our label
+        // and add each detection to the llistbox
+        // with the AV name that detected a positive trigger.
+        private void PrintScan(FileReport fileReport)
+        {
+            int detectedCount = 0;
+
+            if (fileReport.ResponseCode == ReportResponseCode.Present)
+            {
+                // foreach loop for each scan results in fileReport
+                foreach (ScanEngine scan in fileReport.Scans)
+                {
+                    // Checking weather or not there is a detection
+                    // If so we want to add the information to our listbox
+                    bool scangoodbad = scan.Detected;
+
+                    // for debugging purposes we are just checking for 'good' detections or detections = false
+                    if (scangoodbad == false) 
+                    {
+                        detectedCount = Convert.ToInt32(iTalk_Label8.Text);
+                        detectedCount += 1; // for each detection we want to add +1 to our label
+                        iTalk_Label8.Text = detectedCount.ToString();
+                        listBox1.Items.Add(string.Format("AV: {0} | Detected: {1}", scan.Name, scan.Detected));
+                        string scanID = fileReport.ScanId;
+                        iTalk_TextBox_Small3.Text = scanID;
+                        
+                    }
+
+                }
+
+            }
+        }
+    }
+}
